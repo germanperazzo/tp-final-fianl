d47b06b15aa5c11bc27752d988b9d885f16e205f
germanperazzo <128389683+germanperazzo@users.noreply.github.com>
2023-04-14 11:59:36 -0300

    Update Formulario.js

--- Formulario.js
+++ Formulario.js
@@ -28,7 +28,6 @@ const renderizarUsuarios = () => {
 let contadorId = 0
 formularioRegistro.addEventListener("submit", (event) => {
   event.preventDefault()
-  /* console.log(formularioRegistro.nombreCompleto.value) */
   usuarios.push({
     nombre: formularioRegistro.nombreCompleto.value,
     email: formularioRegistro.correoElectronico.value,
@@ -38,6 +37,22 @@ formularioRegistro.addEventListener("submit", (event) => {
   formularioRegistro.reset()
   renderizarUsuarios()
 
-
-
 })
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("formulario").addEventListener('submit', validarFormulario);
+});
+
+function validarFormulario(evento) {
+  evento.preventDefault();
+  var usuario = document.getElementById('usuario').value;
+  if (usuario.length == 0) {
+    alert('No has escrito nada en el usuario');
+    return;
+  }
+  var clave = document.getElementById('clave').value;
+  if (clave.length < 6) {
+    alert('La clave no es válida');
+    return;
+  }
+  this.submit();
+}
